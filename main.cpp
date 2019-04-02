4cf9f4293f539576b2038d39fdf087c56cdf95ec
Jhonatan Macazana <jhonatan.macazana@utec.edu.pe>
2019-04-02 17:26:08 -0500

    Main Code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,38 @@
+#include <cctype>
 #include <iostream>
+#include <algorithm>
+#include <set>
+#include <string>
+#include <map>
 
 using namespace std;
+string Index(string s);
 
-int main() {
-	return EXIT_SUCCESS;
+int main()
+{
+    string word;
+    set<string> words;
+    map<string, int> count;
+    while (cin >> word && word != "#")
+    {
+        words.insert(word);
+        ++count[Index(word)];
+    }
+    for (auto &i : words)
+    {
+        if (count[Index(i)] == 1)
+            cout << i << '\n';
+    }
+    return 0;
 }
 
+string Index(string s)
+{
+  for (auto &i : s)
+  {
+    if (i < 'a')
+        i = tolower(i);
+  }
+  sort(s.begin(), s.end());
+  return s;
+}
